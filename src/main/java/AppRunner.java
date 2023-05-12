a908e556dc39edb36a50e8ee13c56e7b64703e6a
vhumenny <v.humenny@gmail.com>
2023-05-12 23:03:11 +0300

    partly finished

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -4,6 +4,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,13 +14,13 @@ import static java.nio.file.Files.newBufferedWriter;
 
 public class AppRunner {
     private static final String userDirectory = System.getProperty("user.dir") + FileSystems.getDefault().getSeparator();
-    private static final String outputFile = "output.txt";
     private static final Pattern pattern = Pattern.compile("\\w+");
 
     public void start() {
         String bookName = getBookName();
+        if (bookName.equals("exit")) return;
         try (var reader = newBufferedReader(Path.of((userDirectory + bookName)));
-             var writer = newBufferedWriter(Path.of(outputFile))) {
+             var writer = newBufferedWriter(Path.of(userDirectory + bookName + "_statistic.txt"))) {
             List<String> bookStrings = new ArrayList<>();
             String lineToRead = reader.readLine().toLowerCase();
 
@@ -30,14 +31,17 @@ public class AppRunner {
                 }
                 lineToRead = reader.readLine();
             }
-            new WordsStorage().addPopularWords(bookStrings);
-
+            WordsStorage wordsStorage = new WordsStorage();
+            Map<String, Long> result = wordsStorage.processBookWords(bookStrings);
+            for (Map.Entry<String, Long> element : result.entrySet()) {
+                writer.write(element.getKey() + " : " + element.getValue());
+                writer.newLine();
+            }
+            writer.write("total amount of distinct words:" + wordsStorage.getDistinctWordsCounter());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
-
         }
-
     }
 
     private static String getBookName() {
@@ -45,14 +49,10 @@ public class AppRunner {
         Scanner scanner = new Scanner(System.in);
         String bookName = scanner.next();
         File file = new File(userDirectory + bookName);
-        while (!file.exists()) {
-            if (!file.exists()) {
-                System.out.println("please enter correct file name! (example - " + "book.txt");
-            }
-            bookName = scanner.next();
-            file = new File(userDirectory + bookName);
+        if (!file.exists()) {
+            System.out.println("Such book doesn't exist! " +
+                               "Please enter correct file name! (example - " + "book.txt)");
         }
         return bookName;
     }
-
 }
--- src/main/java/WordsStorage.java
+++ src/main/java/WordsStorage.java
@@ -1,15 +1,44 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class WordsStorage {
 
-    private Map<String, Integer> popularWords = new HashMap<>();
+    private final Map<String, Long> popularWords = new HashMap<>();
+    private Long distinctWordsCounter;
 
-
-    public void addPopularWords(List<String> bookStrings) {
-
+    public Map<String, Long> processBookWords(List<String> bookStrings) {
+        getPopularWords(bookStrings);
+        countDistinctWords(bookStrings);
+        printStatisticToConsole();
+        return popularWords;
     }
 
+    private void printStatisticToConsole() {
+        popularWords.forEach((key, value) -> System.out.println(key + " : " + value));
+        System.out.println("total amount of distinct words:" + distinctWordsCounter);
+    }
 
+    private void countDistinctWords(List<String> bookStrings) {
+        distinctWordsCounter = bookStrings.stream()
+                .filter(s -> s.length() > 2)
+                .distinct()
+                .count();
+    }
+
+    private void getPopularWords(List<String> bookStrings) {
+        bookStrings.stream()
+                .filter(s -> s.length() > 2)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(10)
+                .forEach(entry -> popularWords.put(entry.getKey(), entry.getValue()));
+    }
+
+    public Long getDistinctWordsCounter() {
+        return distinctWordsCounter;
+    }
 }
